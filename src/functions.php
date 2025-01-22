b9b1b6a80e542216c2699b000144f98671ab9d0a
Vladimir Barbaros <vladimir.b@bannernow.com>
2025-01-22 07:31:06 +0200

    better filtering for snippets

--- src/functions.php
+++ src/functions.php
@@ -179,6 +179,87 @@ function laravel_debug_eval($options = [])
     <script src="https://unpkg.com/vue@2.7.14/dist/vue.js"></script>
     <script src="https://unpkg.com/jquery@3.6.1/dist/jquery.js"></script>
     <script>
+        // https://github.com/vbarbarosh/node-helpers/blob/efee928c2ea20eb45eb559cdc976b0eb34a0e2b5/src/filter1_from_spec.js
+(function () {
+
+    window.filter1_from_spec = filter1_from_spec;
+
+    /**
+     * Creates a filtering function based on the following spec:
+     * 1. Substrings are separated by `/`.
+     * 2. Each substring may have special characters:
+     *    - `^`: Substring must appear at the beginning.
+     *    - `$`: Substring must appear at the end.
+     *    - `!`: Negates the condition (substring must not appear).
+     */
+    function filter1_from_spec(spec)
+    {
+        let a, b, c, d;
+        const parts = parse_spec(spec);
+        switch (parts.length) {
+        case 0:
+            // No filters, always true
+            return () => true;
+        case 1:
+            return parts[0];
+        case 2:
+            [a, b] = parts;
+            return s => a(s) && b(s);
+        case 3:
+            [a, b, c] = parts;
+            return s => a(s) && b(s) && c(s);
+        case 4:
+            [a, b, c, d] = parts;
+            return s => a(s) && b(s) && c(s) && d(s);
+        default:
+            return s => parts.every(fn => fn(s));
+        }
+    }
+
+    function parse_spec(spec)
+    {
+        return spec.split('/').filter(v => v).map(parse_expr);
+    }
+
+    // convert expr into an array of objects {substr, starts, ends}
+    function parse_expr(expr)
+    {
+        let substr;
+        let starts = false; // ^
+        let ends = false; // $
+        substr = expr.replaceAll('^', '');
+        starts = (substr.length !== expr.length);
+        expr = substr;
+        substr = expr.replaceAll('$', '');
+        ends = (substr.length !== expr.length);
+        expr = substr;
+        substr = expr.replaceAll('!', '');
+        if ((expr.length - substr.length) % 2) { // not
+            if (starts && ends) {
+                return s => !s.startsWith(substr) && !s.endsWith(substr);
+            }
+            if (starts) {
+                return s => !s.startsWith(substr);
+            }
+            if (ends) {
+                return s => !s.endsWith(substr);
+            }
+            return s => !s.includes(substr);
+        }
+        if (starts && ends) {
+            return s => s.startsWith(substr) && s.endsWith(substr);
+        }
+        if (starts) {
+            return s => s.startsWith(substr);
+        }
+        if (ends) {
+            return s => s.endsWith(substr);
+        }
+        return s => s.includes(substr);
+    }
+})();
+    </script>
+    <script>
         (function () {
             const listeners = [];
             const listeners_jquery = {
@@ -393,10 +474,8 @@ function laravel_debug_eval($options = [])
                     return Object.values(unique);
                 },
                 snippets: function () {
-                    const _this = this;
-                    return this.snippets_orig.filter(function (snippet) {
-                        return snippet.title.includes(_this.filter.toLowerCase());
-                    });
+                    const filter = filter1_from_spec(this.filter.toLowerCase());
+                    return this.snippets_orig.filter(v => filter(v.title));
                 },
                 auto_refresh_running_time: function () {
                     if (!this.auto_refresh.started_at) {
